32f1c283f1be276318af8caf7c0a81e5c27ba839
¨joaquin <joakig6@gmail.com>
2024-05-27 21:15:08 -0300

    render de navbar con js , para ahorrar codigo  y escalable para otros html

---
+++ js/renderNavBar.js
@@ -0,0 +1,52 @@
+const renderNavBar = () => {
+    const currentPath = window.location.pathname;
+    const isInIndex = currentPath.includes("index.html") || currentPath === "/";
+    const generateHref = (isInIndex, path) => {
+        let href = `./${path}`;
+
+        if (isInIndex) {
+            href = `./html/${path}`;
+        }
+        return href
+    }
+    const navBar = document.getElementById("headerContainer");
+    const renderLogoPath = isInIndex?"./imagenes/Logo Tuki.jpg":"../imagenes/Logo Tuki.jpg";
+    navBar.innerHTML = `
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="../index.html"><img src= "${renderLogoPath}" alt="#"></a>
+          <span id="user-greeting" style="color: aliceblue; text-decoration: underline; font-weight: 600;"></span>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="${generateHref(isInIndex, "productos.html")}">Productos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="${generateHref(isInIndex, "acercade.html")}">Acerca de</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="${generateHref(isInIndex, "contacto.html")}">Contacto</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="${generateHref(isInIndex, "consumoAPI.html")}">Recomendados</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="${generateHref(isInIndex, "Registro.html" )}">Registrarse</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="${generateHref(isInIndex, "iniciar-sesion.html")}">Iniciar Sesion</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="${generateHref(isInIndex, "index.html")}" onclick="cerrarSesion()">Cerrar Sesion</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    `;
+}
+
+renderNavBar()
